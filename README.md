b4db8e3e1efa2086c1d7793ed0e008dbd29bf332
primaru1948 <geicarobert@gmail.com>
2020-02-25 16:11:05 +0200

    a lot of changes

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
 ### TODO:
-- [ ] add three options to read: read, unread, reading
-- [ ] optional: add isbn, completed pages
-- [ ] do not allow to write letters on 'pages' input
-- [ ] add image from url
-- [ ] add popup at invalid input
+- [x] add three options to status: read, unread, reading
+- [x] add completed pages
+- [x] do not allow to write letters on 'pages' input
+- [x] add popup at invalid input
 - [x] implement delete book
 - [x] implement update
 - [ ] implement search
-- [ ] implement ratings
 - [ ] implement sorting
 - [ ] add infos page (total books, completed books, total pages, total completed pages)
-
-
+- [ ] add authentification
--- script.js
+++ script.js
@@ -19,20 +19,42 @@ let databaseRef = database.ref('books');
 // event value will have two callbacks
 databaseRef.on('value', getData, errorData);
 
-// let myLibrary = [];
-
 // book constructor
-function Book(title, author, pages, read) {
+function Book(title, author, pages, completedPages, status) {
     this.id = Date.now();
     this.title = title;
     this.author = author; 
     this.pages = pages;
-    this.read = read;
-    // this.info = function() {
-    //     return `${title} by ${author}, ${pages}, ${read}`;
-    // }
+    this.completedPages = completedPages;
+    this.status = status;
 }
 
+
+let addBookButtonModal = document.getElementById('addBookButton-modal'); //the button which open add modal 
+let addBookModal = document.getElementById('addBook-modal'); // the modal
+let closeAddModal = document.getElementsByClassName("closeModal")[0]; // the close modal button
+
+// display modal on btn click
+addBookButtonModal.onclick = () => {
+    addBookModal.style.display = 'block';
+    console.log('modal on screen');
+}
+
+// hide modal on 'X' click
+closeAddModal.onclick = () => {
+    addBookModal.style.display = 'none';
+    console.log('modal off screen (x)');
+}
+
+// hide modal on window click
+window.onclick = (e) => {
+    if(e.target == addBookModal) {
+        addBookModal.style.display = 'none';
+        console.log('modal off screen (w)');
+    }
+}
+
+// add book function
 const addBookToLibrary = (e) => {
     e.preventDefault();
 
@@ -40,29 +62,43 @@ const addBookToLibrary = (e) => {
     let bookTitle = document.getElementById('bookTitle').value;
     let bookAuthor = document.getElementById('bookAuthor').value;
     let bookPages = document.getElementById('bookPages').value;
-    let bookRead = document.getElementById('bookRead').value;
+    let bookCompletedPages = document.getElementById('bookCompletedPages').value;
+    let bookStatus = document.getElementById('bookStatus').value;
 
-    // check for empty form
-    if(bookTitle !== "" && bookAuthor !== "" && !isNaN(bookPages) && bookPages !== "") {
-        let myBook = new Book(bookTitle, bookAuthor, bookPages, bookRead);
+    // check for empty form or non number at pages
+    if(bookTitle !== "" && bookAuthor !== "" && !isNaN(bookPages) && bookPages !== "" && !isNaN(bookCompletedPages) && bookCompletedPages !== '') {
+        // create object
+        let myBook = new Book(bookTitle, bookAuthor, bookPages, bookCompletedPages, bookStatus);
+
         // push myBook object to database
         databaseRef.push(myBook);
-        console.log('Pushed to database')
+        
+        // feedback to user
+        Swal.fire(
+            'Succes!',
+            `${bookTitle} has been added!`,
+            'success'
+        );
     } else {
-        console.log('Complete form correctly')
+        Swal.fire({
+            icon: 'error',
+            title: 'Error!',
+            text: 'You typed something wrong!'
+        });
     }
 
 }
 
 // trigger function on btn click
-let submitButton = document.getElementById('btn');
-submitButton.addEventListener('click', addBookToLibrary);
+let submitBookBtn = document.getElementById('submitBookBtn');
+submitBookBtn.addEventListener('click', addBookToLibrary);
+
 
 // retrieve data from database
 function getData(data) {
-    // select all li elements with listingBook class
+    // select all rows with listingBook class
     let listingBooks = document.getElementsByClassName("listingBook");
-    // iterate through all elements with listingBook class
+    // iterate through all rows with listingBook class
     for(let i=0; i<listingBooks.length; i++) {
         listingBooks[i].style.display = 'none'; // hide to prevent multiplication
     }
@@ -73,89 +109,161 @@ function getData(data) {
     let keys = Object.keys(books);
     
     // iterate through the database keys
-    let title, author, pages, read, id;
+    let title, author, pages, completedPages, status, id;
+
     for(let i=0; i<keys.length; i++) {
         let k = keys[i];
         title = books[k].title;
         author = books[k].author;
         pages = books[k].pages;
-        read = books[k].read;
+        completedPages = books[k].completedPages;
+        status = books[k].status;
         id = books[k].id;
 
         // display on page
-        const ul = document.createElement('ul');
-        const li = document.createElement('li');
-        const cross = document.createElement('div');
-        const updateButton = document.createElement('button');
+        const deleteButton = document.createElement('div');
+        let updateButton = document.createElement('div');
 
-        // add listingBook class to all elements from list
-        li.classList.add('listingBook');
-        // add key as data atribute to every list element
-        li.setAttribute('data-id', k);
-        // set content of cross to 'X'
-        cross.textContent = 'X';
-        // set content of update button to 'update'
-        updateButton.textContent = 'update';
+        // add deleteIcon class and icon to deletebutton
+        deleteButton.classList.add('deleteIcon');
+        deleteButton.classList.add('fas');
+        deleteButton.classList.add('fa-trash-alt');
 
-        // append li elements to ul and set style to none
-        ul.appendChild(li);
-        ul.style = 'list-style-type: none';
+        // add updateBtn class and icon to updateButton
+        updateButton.classList.add('updateBtn');
+        updateButton.classList.add('fas');
+        updateButton.classList.add('fa-edit');
 
-        // show title, author, pages, read and id on page
-        li.innerHTML = `${title} by ${author}, ${pages}, ${read} with id ${id}`;
-        // append ol to body
-        document.body.appendChild(ul);
-        // append cross and update to li
-        li.appendChild(cross);
-        li.appendChild(updateButton);
+        
+        // display data to a table
+        let table = document.getElementById('booksTable');
+        let row = table.insertRow(-1);
+
+        // add key as data atribute to every row
+        row.setAttribute('data-id', k);
+        // add listingBook class to every row to prevent multiplication
+        row.classList.add('listingBook'); 
+
+        // cell 0 => update button
+        let cell0 = row.insertCell(0);
+        cell0.appendChild(updateButton);
+
+        // cell 1 => title
+        let cell1 = row.insertCell(1);
+        cell1.innerHTML = `${title}`;
+
+        // cell 2 => author
+        let cell2 = row.insertCell(2);
+        cell2.innerHTML = `${author}`;
+
+        // cell 3 => pages
+        let cell3 = row.insertCell(3);
+        cell3.innerHTML = `${pages}`;
+        
+        // cell 4 => completed pages
+        let cell4 = row.insertCell(4);
+        cell4.innerHTML = `${completedPages}`;
+
+        // cell 5 => status
+        let cell5 = row.insertCell(5);
+        cell5.innerHTML = `${status}`;
+
+        //cell 6 => id
+        let cell6 = row.insertCell(6);
+        cell6.innerHTML = `${id}`;
+
+        // cell 7 => delete button
+        let cell7 = row.insertCell(7);        
+        cell7.appendChild(deleteButton);
+
+
 
         // delete book from database on cross click
-        cross.addEventListener('click', (e) => {
+        deleteButton.addEventListener('click', (e) => {
             e.stopPropagation();
-            // get key on cross click
-            let bookKey = e.target.parentElement.getAttribute('data-id'); 
+
+            // get key on deleteButton click 
+            let bookKey = row.getAttribute('data-id'); 
+            console.log(row.getAttribute('data-id'));
+
             // get reference to database key
             let keyRef = database.ref('books').child(bookKey);
 
-            keyRef.remove().then(function() {
-                console.log('deleted successfully');
-            }).catch(function(error) {
-                console.log('error happen on delete');
-            });
-        });
+            // get confirmation on alert to delete
+            Swal.fire({
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+            // if delete is confirmed
+            }).then((result) => {
+                if (result.value) {
+                    // remove key from database
+                    keyRef.remove().then(function() {
+                        // deleted successfuly
+                    }).catch(function(error) {
+                        // error alert
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Error!',
+                            text: `Error: ${error}`
+                        });
+                    });
 
+                    // delete alert
+                    Swal.fire(
+                        'Deleted!',
+                        `${books[k].title} has been deleted.`,
+                        'success'
+                    );
+                }
+            });
+            
+        });
 
         // update book 
         updateButton.addEventListener('click', (e) => {
+            e.stopPropagation();
             
-
-            let modal = document.getElementById('updateModal');
-            let modalBtn = document.getElementById('modalBtn');
-            let span = document.getElementsByClassName('close')[0];
+            let modal = document.getElementById('update-modal');
+            let closeUpdateModal = document.getElementsByClassName('closeModal')[1];
             let updateBookButton = document.getElementById('updateBookButton');
 
             // display modal
             modal.style.display = 'block';
 
-            // close modal when user clicks on span
-            span.onclick = function() {
+            // close modal when user clicks on 'X'
+            closeUpdateModal.onclick = () => {
                 modal.style.display = 'none';
+                console.log('hide on x click')
             }
 
-            // get key on cross click
-            let bookKey = e.target.parentElement.getAttribute('data-id'); 
+            // close modal when user clicks on windows
+            window.onclick = (e) => {
+                if(e.target == modal) {
+                    modal.style.display = 'none';
+                    console.log('hide on w click');
+                }
+            }
+
+            // get key on update btn click 
+            let bookKey = row.getAttribute('data-id');
             // get reference to database key
             let keyRef = database.ref('books').child(bookKey);
-            
-            // console.log(books[k].title);
+                
             // update book record from modal form
             updateBookButton.onclick =  () => {
                 keyRef.update({
                     title: document.getElementById('updateTitle').value,
                     author: document.getElementById('updateAuthor').value,
                     pages: document.getElementById('updatePages').value,
-                    read: document.getElementById('updateRead').value
+                    completedPages: document.getElementById('updateCompletedPages').value,
+                    status: document.getElementById('updateStatus').value
                 });
+
             };
 
         });
@@ -164,20 +272,23 @@ function getData(data) {
 
 
 function errorData(error) {
-    console.log('something happen: ' + error);
+    Swal.fire({
+        icon: 'error',
+        title: 'Error!',
+        text: `Error: ${error}`
+    });
 }
 
 
 /* TODO: 
-
-    optional: add isbn, completed pages
-    do not allow to write letters on pages form
-    add image from url
-    add popup when invalid input
-    implement update
-    implement search
-    implement ratings
-    implement sorting
-    add informations page (total books, completed books, total pages, total completed pages)
-
+    1. update name on update modal
+    2. establish the design of books listing
+    3. do not allow to write letters on pages form
+    4. add popup when invalid input
+    5. add isbn
+    6. completed pages
+    7. add image from url
+    8. add informations page (total books, completed books, total pages, total completed pages)
+    9. implement search
+    10. implement sorting
 */
